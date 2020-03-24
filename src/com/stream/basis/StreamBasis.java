6fc762a28ff42928664faff14e6d14827a272447
kn52 <ashish52922@gmail.com>
2020-03-24 12:31:46 +0530

    ADDED:Stream array

---
+++ src/com/stream/basis/StreamBasis.java
@@ -0,0 +1,14 @@
+package com.stream.basis;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static java.lang.System.out;
+
+public class StreamBasis {
+
+	public static void main(String[] args) {
+		List<Integer> integer=new ArrayList(Arrays.asList(1,2,3,4,5,6));
+		out.println(integer);
+	}
+}
